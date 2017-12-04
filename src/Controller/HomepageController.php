7c92c43bae34fed1060899fe39e829bc30b6518d
Adrien H <nuance.rouille@gmail.com>
2017-12-04 20:06:27 +0100

    [add][!dev] MariaDB configurations

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,13 +4,20 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class HomepageController
+ * @package App\Controller
+ */
 class HomepageController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('homepage.html.twig');
     }
---
+++ src/Entity/Post.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=31)
+     */
+    private $name;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return self
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param null|string $name
+     * @return self
+     */
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
