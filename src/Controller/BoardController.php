e7185a4a9776a801b2c28b439d23b8a222a2c97c
Adrien H <nuance.rouille@gmail.com>
2018-01-04 11:07:09 +0100

    [hotfix] 500 error on new topic

--- src/Controller/BoardController.php
+++ src/Controller/BoardController.php
@@ -8,6 +8,7 @@ use App\Entity\Protagonist;
 use App\Entity\Topic;
 use App\Entity\User;
 use App\Form\TopicType;
+use App\Security\Voter\BoardVoter;
 use App\Utils\BBPlus;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,12 +41,15 @@ class BoardController extends Controller
     /**
      * @Route("/{id}/add-topic", name="board.add-topic", requirements={"id"="\d+"})
      *
+     *
      * @param Board $board
      * @param Request $request
      * @return Response
      */
     public function addTopic(Board $board, Request $request): Response
     {
+        $this->denyAccessUnlessGranted(BoardVoter::ADD_TOPIC, $board);
+
         $topic = new Topic();
         $topic->setBoard($board);
         $form = $this->createForm(TopicType::class, $topic);
@@ -55,9 +59,13 @@ class BoardController extends Controller
             $em = $this->getDoctrine()->getManager();
             $topic = $form->getData();
             $post = $form->get('post')->getData();
-            // todo $this->getUser()
-            $post->setUser($em->getRepository(User::class)->find(1));
-//            $post->setProtagonist($em->getRepository(Protagonist::class)->find(1));
+            $post->setUser($this->getUser());
+
+            // @Todo Behaviour
+            $now = new \DateTime();
+            $post->setCreatedAt($now);
+            $post->setUpdatedAt($now);
+
             $topic->addPost($post);
             $em->persist($post);
             $em->persist($topic);
